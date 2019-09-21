407163e02b3e1992df54bedd77dfcb729672a96a
mika <support@unitycoder.com>
2019-09-21 09:57:34 +0300

    add datagrid context menu, add Unitys-tab items, add UnityInstallations class

---
+++ UnityLauncherPro/UnityInstallations.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UnityLauncherPro
+{
+    public class UnityInstallations
+    {
+        public string Version { set; get; }
+        public string Path { set; get; }
+        public DateTime Installed { set; get; }
+        public string Platforms { set; get; }
+    }
+}
