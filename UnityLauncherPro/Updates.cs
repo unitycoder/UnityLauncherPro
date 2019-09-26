049d68f6b11b030cb4dfa80e4b55d9b3cb3f76d7
mika <support@unitycoder.com>
2019-09-26 20:55:28 +0300

    add checkboxes to settings tab, add Updates class

---
+++ UnityLauncherPro/Updates.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace UnityLauncherPro
+{
+    public class Updates
+    {
+        public string Version { set; get; }
+        public DateTime ReleaseDate { set; get; }
+    }
+}
