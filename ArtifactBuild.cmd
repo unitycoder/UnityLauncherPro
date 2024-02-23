07b70ae6001824dc568b7fe9c165018a1487f0d9
mika <unitycoder@users.noreply.github.com>
2024-02-23 22:39:49 +0200

    Update ArtifactBuild.cmd #TEST

--- ArtifactBuild.cmd
+++ ArtifactBuild.cmd
@@ -1,6 +1,6 @@
 @echo off
 pushd "%~dp0"
-powershell Compress-7Zip "Bin\Release" -ArchiveFileName "UnityLauncherPro.zip" -Format Zip
+powershell Compress-7Zip "UnityLauncherPro\bin\Release\UnityLauncherPro.exe" -ArchiveFileName "UnityLauncherPro.zip" -Format Zip
 :exit
 popd
 @echo on
