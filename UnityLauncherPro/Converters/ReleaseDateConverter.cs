ca48d5759e27a4937bceb2cf4853d821ee8e02f0
unitycoder <support@unitycoder.com>
2024-11-07 19:43:46 +0200

    fix converter issue (in design time),

--- UnityLauncherPro/Converters/ReleaseDateConverter.cs
+++ UnityLauncherPro/Converters/ReleaseDateConverter.cs
@@ -1,29 +1,49 @@
 ﻿using System;
 using System.Globalization;
+using System.Windows;
 using System.Windows.Data;
 
 namespace UnityLauncherPro.Converters
 {
-    [ValueConversion(typeof(DateTime), typeof(String))]
+    [ValueConversion(typeof(DateTime), typeof(string))]
     public class ReleaseDateConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            if (value == null) return null;
-            DateTime date = (DateTime)value;
+            if (value == null || !(value is DateTime date))
+            {
+                return DependencyProperty.UnsetValue;
+            }
 
-            // get first part of string until space character (updates only contain mm/dd/yyyy)
-            string dateStrTrimmed = MainWindow.currentDateFormat;
-            if (dateStrTrimmed.IndexOf(' ') > -1) dateStrTrimmed = dateStrTrimmed.Split(' ')[0];
+            // Use a default date format if currentDateFormat is null or empty
+            string dateStrTrimmed = MainWindow.currentDateFormat ?? "MM/dd/yyyy";
 
-            return MainWindow.useHumanFriendlyDateFormat ? Tools.GetElapsedTime(date) : date.ToString(dateStrTrimmed);
+            // If the format includes time, use only the date portion
+            if (dateStrTrimmed.Contains(" "))
+            {
+                dateStrTrimmed = dateStrTrimmed.Split(' ')[0];
+            }
+
+            // Return a human-friendly format if enabled; otherwise, format based on dateStrTrimmed
+            return MainWindow.useHumanFriendlyDateFormat
+                ? Tools.GetElapsedTime(date)
+                : date.ToString(dateStrTrimmed, culture);
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            // not used ?
-            return DateTime.ParseExact((string)value, MainWindow.currentDateFormat, culture);
-        }
+            if (value == null || string.IsNullOrWhiteSpace(value.ToString()))
+            {
+                return DependencyProperty.UnsetValue;
+            }
 
+            // Attempt to parse back to DateTime using the specified format
+            if (DateTime.TryParseExact((string)value, MainWindow.currentDateFormat ?? "MM/dd/yyyy", culture, DateTimeStyles.None, out DateTime parsedDate))
+            {
+                return parsedDate;
+            }
+
+            return DependencyProperty.UnsetValue;
+        }
     }
 }
