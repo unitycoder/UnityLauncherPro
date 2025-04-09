ee7cb29287cdc9c834692ebe9710bfd9a599baf9
unitycoder <support@unitycoder.com>
2025-04-09 23:04:28 +0300

    move crash recovery scene fixes #186

--- UnityLauncherPro/Tools.cs
+++ UnityLauncherPro/Tools.cs
@@ -400,7 +400,7 @@ namespace UnityLauncherPro
             var recoveryFile = Path.Combine(projectPath, "Temp", "__Backupscenes", "0.backup");
             if (File.Exists(recoveryFile))
             {
-                var result = MessageBox.Show("Crash recovery scene found, do you want to copy it into Assets/_Recovery/-folder?", "UnityLauncherPro - Scene Recovery", MessageBoxButton.YesNo, MessageBoxImage.Question);
+                var result = MessageBox.Show("Crash recovery scene found, do you want to MOVE it into Assets/_Recovery/-folder?", "UnityLauncherPro - Scene Recovery", MessageBoxButton.YesNo, MessageBoxImage.Question);
                 if (result == MessageBoxResult.Yes)
                 {
                     var restoreFolder = Path.Combine(projectPath, "Assets", "_Recovery");
@@ -412,7 +412,22 @@ namespace UnityLauncherPro
                     {
                         Int32 unixTimestamp = (Int32)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
                         var uniqueFileName = "Recovered_Scene" + unixTimestamp + ".unity";
-                        File.Copy(recoveryFile, Path.Combine(restoreFolder, uniqueFileName));
+                        
+                        try
+                        {
+                            File.Move(recoveryFile, Path.Combine(restoreFolder, uniqueFileName));
+                            // remove folder, otherwise unity 6000.2 asks for recovery
+                            Directory.Delete(Path.Combine(projectPath, "Temp", "__Backupscenes"), true);
+
+                            Console.WriteLine("moved file to "+ uniqueFileName);
+                        }
+                        catch (IOException)
+                        {
+                            // if move failed, try copy
+                            File.Copy(recoveryFile, Path.Combine(restoreFolder, uniqueFileName));
+                            Console.WriteLine("copied file");
+                        }
+
                         Console.WriteLine("Recovered crashed scene into: " + restoreFolder);
                     }
                     else
@@ -2746,7 +2761,7 @@ public static class UnityLauncherProTools
                 {
                     MessageBox.Show("Operation cancelled or failed due to insufficient privileges.", "Cancelled", MessageBoxButton.OK, MessageBoxImage.Warning);
                 }
-                    return false;
+                return false;
             }
 
             return true;
